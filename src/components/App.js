5bdb319b6f67f22473d3f4b32f02a9f3f6f95db3
purnankjain <purnank_jain@yahoo.com>
2018-06-14 01:51:52 +0530

    [Purnank] | Boiler plate

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../styles/css/App.css';
+
+class App extends Component {
+  render() {
+    return (<p>Predictor</p>);
+  }
+}
+
+export default App;
